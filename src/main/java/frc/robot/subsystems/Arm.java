e3854f9fd2df0c502c9e94c8c40aa87d14bec6eb
Retro-5ive <retro5five6914@gmail.com>
2024-02-14 15:08:37 -0500

    For Ricky :)

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,13 +5,246 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
+
+
 
 public class Arm extends SubsystemBase {
-  /** Creates a new Arm. */
-  public Arm() {}
 
+  private boolean isReadyToRecieve = false;
+
+ 
+  private CANSparkMax Rotate1;
+  private CANSparkMax Rotate2;
+
+  private static Arm _instance;
+  private AbsoluteEncoder Rotate1Encoder;
+  private AbsoluteEncoder Rotate2Encoder;
+
+  // PIDS!
+  private SparkMaxPIDController Rotate2_pidController;
+  private SparkMaxPIDController Rotate1_pidController;
+
+  private RelativeEncoder backupencoder;
+
+  private static final int kCPR = 8192;
+  
+  /** Creates a new Arm. */
+  public Arm() {
+    
+    Rotate1 = new CANSparkMax(Constants.ArmConstants.Rotate1ID, MotorType.kBrushless);
+    Rotate2 = new CANSparkMax(Constants.ArmConstants.Rotate2Id, MotorType.kBrushless);
+    
+    Rotate1.restoreFactoryDefaults();
+   
+    Rotate2.restoreFactoryDefaults();
+
+    
+    Rotate1.setInverted(true);
+
+    
+
+    //Arm encoders
+    Rotate1Encoder = Rotate1.getAbsoluteEncoder(Type.kDutyCycle);
+    Rotate2Encoder.setInverted(false);
+
+    Rotate1Encoder = Rotate2.getAbsoluteEncoder(Type.kDutyCycle);
+    Rotate2Encoder.setInverted(true);
+
+
+    backupencoder = Rotate2.getEncoder();
+    backupencoder.setPosition(0);
+    
+    Rotate1Encoder.setZeroOffset(45/360);
+    // PID Controllers
+
+    // Rotate2 PID
+    Rotate2_pidController = Rotate2.getPIDController();
+    Rotate2_pidController.setFeedbackDevice(Rotate2Encoder);
+    Rotate2_pidController.setP(Constants.ArmConstants.Rotate2UpP);
+    Rotate2_pidController.setI(Constants.ArmConstants.Rotate2I);
+    Rotate2_pidController.setD(Constants.ArmConstants.Rotate2D);
+    Rotate2_pidController.setOutputRange(-1.0, 1.0);
+
+    
+
+    // Rotate1 PID
+    Rotate1_pidController = Rotate1.getPIDController();
+    Rotate1_pidController.setFeedbackDevice(Rotate1Encoder);
+    Rotate1_pidController.setP(Constants.ArmConstants.Rotate1UpP);
+    Rotate1_pidController.setI(Constants.ArmConstants.Rotate1I);
+    Rotate1_pidController.setD(Constants.ArmConstants.Rotate1D);
+    
+    Rotate1_pidController.setOutputRange(-1.0, 1.0);
+
+
+    // Brake mode
+    
+    Rotate1.setIdleMode(IdleMode.kBrake);
+    Rotate1.setIdleMode(IdleMode.kBrake);
+    Rotate1.setClosedLoopRampRate(0.25);
+
+    //reset encoders
+   
+
+
+  }
+
+  public static Arm getInstance() {
+    if (_instance == null) {
+      _instance = new Arm();
+    }
+    return _instance;
+  }
+
+  
+/**
+ * Sets Rotate1 to a power.
+ * @param power power of elbow
+ */
+  public void setRotate1(double power) {
+    Rotate1.set(power *.7);
+  }
+
+/**
+ * Stops Rotate1.
+ */
+  public void stopRotate1() {
+    Rotate1.set(0);
+  }
+/**
+ * @return Rotate1 Encoder in Degrees
+ */
+  public double getRotate1Clicks() {
+    return Rotate1Encoder.getPosition()*360;
+  }
+
+  /**
+   * Gets Rotate2 Encoder Position.
+   */
+  public double getRotate2Clicks() {
+    return Rotate2Encoder.getPosition();
+  }
+
+  // Rotate2
+
+  /**
+   * @param power Power of Rotate2
+   */
+  public void moveRotate2(double power) {
+    Rotate2.set(power);
+  }
+
+  public void stopRotate2() {
+    Rotate2.set(0);
+  }
+
+ /**
+  * Uses PID looping to set the Rotate2 to a certain position 
+  * @param refrence Position to send Rotate2 to 
+  */
+  public void clawTo(double refrence) {
+    Rotate2_pidController.setReference(refrence, ControlType.kPosition);
+    
+  }
+
+  /**
+   * Uses PID looping to set the Rotate1 to a certain position 
+   * @param refrence Position to send Rotate1 to 
+   */
+  public void Rotate1To(double refrence) {
+    Rotate1_pidController.setReference(refrence/360, CANSparkMax.ControlType.kPosition);
+  }
+
+
+/**
+ * Sets the PID values for Rotate1 going up
+ */
+  public void setRotate1PID() {
+    Rotate1_pidController.setP(Constants.ArmConstants.Rotate1UpP);
+    Rotate1_pidController.setI(Constants.ArmConstants.Rotate1I);
+    Rotate1_pidController.setD(Constants.ArmConstants.Rotate1D);
+  }
+  /**
+   * Sets the PID value for the Rotate1 when it goes down
+   */
+  public void setRotate2PID() {
+    Rotate2_pidController.setP(Constants.ArmConstants.Rotate2DownP);
+    Rotate2_pidController.setI(Constants.ArmConstants.Rotate2I);
+    Rotate2_pidController.setD(Constants.ArmConstants.Rotate2D);
+  }
+
+  public void setReadyToRecieve() {
+    isReadyToRecieve = true;
+  }
+
+  public void notReadyToRecieve() {
+    isReadyToRecieve = false;
+  }
+/**
+ * @return if the robot is in the ready to recieve position
+ */
+  public boolean getReadyToRecieve() {
+    return isReadyToRecieve;
+  }
+/**
+*puts the encoder positions for the Rotate1 and the Rotate2 on the smart dashboard
+*/
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Rotate1Encoder", getRotate1Clicks());
+    SmartDashboard.putNumber("Rotate2Encoder", getRotate2Clicks());
+    SmartDashboard.putNumber("Rotate2 current", Rotate2.getOutputCurrent());
+    
+    
   }
 }
+
+
+
+
+
+
+
+ /* 
+   * Declaring the Pigeon 2 gyro. Creating the variables to recieve yaw and pitch
+   * values from the sensor
+   * 
+   * Declaring the CANcoder for the Falcon 500 running the arm.
+   */
+  /*double getArmPos = 0;
+
+  CANCoder armSensor = new CANCoder(1);
+ 
+  public double getPosition() {
+    return getArmPos;
+  }
+ public void MotorLimit(){
+  if (arm.get() > 170){
+
+  arm.stopMotor();
+
+   } else if(arm.get() > ){
+arm.stopMotor();
+  }
+}
+*/
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,42 +5,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
-
-   /**
-   * Puts hinge encoder positions on smartdashboard.
-   * Puts intake sensor state on smartdashboard.
-   */
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    
-    SmartDashboard.putNumber("Hinge Position", getEncoder());
-    SmartDashboard.putBoolean("intake sensor", getIntakeSensor());
-  }
-}
-    // This method will be called once per scheduler run
-  }
-}
-
-
-
-
-
-
-
-
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
@@ -49,16 +20,14 @@ import edu.wpi.first.math.trajectory.Trajectory.State;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
 public class Intake extends SubsystemBase {
 
-  // Intake arms that move up and down
-  CANSparkMax leftHinge;
-  CANSparkMax rightHinge;
-  AbsoluteEncoder hingEncoder;
-  StateController stateController;
+
+  // Intake arms that open and close
+  CANSparkMax intakeInhale;
+  CANSparkMax intakeExhale;
+  AbsoluteEncoder revboreEncoder ;
   private static Intake _instance;
 
   private DigitalInput sensor;
@@ -70,94 +39,113 @@ public class Intake extends SubsystemBase {
   // Spaghetti wheels
   WPI_TalonSRX intake;
 
+  /** Creates a new Intake. */
   public Intake() {
-    stateController = StateController.getInstance();
     toggledUp = true;
     // Hinges
-    leftHinge = new CANSparkMax(Constants.IntakeConstants.leftHingeId, MotorType.kBrushless);
-    rightHinge = new CANSparkMax(Constants.IntakeConstants.rightHingeId, MotorType.kBrushless);
+    intakeInhale = new CANSparkMax(Constants.IntakeConstants.intakeInhaleId, MotorType.kBrushless);
+    intakeExhale = new CANSparkMax(Constants.IntakeConstants.intakeExhaleId, MotorType.kBrushless);
+
+
+    intakeInhale.restoreFactoryDefaults();
+    intakeExhale.restoreFactoryDefaults();
+    intakeInhale.setInverted(true);
+    
+    intakeExhale.follow(intakeInhale, true);
+
+    intakeExhale.setIdleMode(IdleMode.kBrake);
+    intakeInhale.setIdleMode(IdleMode.kBrake);
+
     
     
-    leftHinge.restoreFactoryDefaults();
-    rightHinge.restoreFactoryDefaults();
-    leftHinge.setInverted(true);
-    
-    rightHinge.follow(leftHinge, true);
 
-    rightHinge.setIdleMode(IdleMode.kBrake);
-    leftHinge.setIdleMode(IdleMode.kBrake);
+    revboreEncoder = intakeInhale.getAbsoluteEncoder(Type.kDutyCycle);
+    revboreEncoder.setInverted(true);
 
-    
-    
-
-    hingEncoder = leftHinge.getAbsoluteEncoder(Type.kDutyCycle);
-    hingEncoder.setInverted(true);
-
-    // PIDS!
-    hingePIDController = leftHinge.getPIDController();
-    hingePIDController.setFeedbackDevice(hingEncoder);
-    hingePIDController.setP(Constants.IntakeConstants.hingeP);
-    hingePIDController.setI(Constants.IntakeConstants.hingeI);
-    hingePIDController.setD(Constants.IntakeConstants.hingeD);
-    hingePIDController.setOutputRange(-1.0, 1.0);
-    leftHinge.setClosedLoopRampRate(0.5);
-    rightHinge.setClosedLoopRampRate(0.5);
+  // PIDS!
+  hingePIDController = intakeInhale.getPIDController();
+  hingePIDController.setFeedbackDevice(revboreEncoder);
+  hingePIDController.setP(Constants.IntakeConstants.hingeP);
+  hingePIDController.setI(Constants.IntakeConstants.hingeI);
+  hingePIDController.setD(Constants.IntakeConstants.hingeD);
+  hingePIDController.setOutputRange(-1.0, 1.0);
+  intakeInhale.setClosedLoopRampRate(0.5);
+  intakeExhale.setClosedLoopRampRate(0.5);
 
 
-    // Spaghetti
-    intake = new WPI_TalonSRX(Constants.IntakeConstants.intakeId);
-    intake.enableVoltageCompensation(true);
+  // Spaghetti
+  intake = new WPI_TalonSRX(Constants.IntakeConstants.intakeId);
+  intake.enableVoltageCompensation(true);
 
-    sensor = new DigitalInput(Constants.IntakeConstants.sensorId);
+  sensor = new DigitalInput(Constants.IntakeConstants.sensorId);
+}
+
+
+
+public static Intake getInstance() {
+  if (_instance == null) {
+          _instance = new Intake();
   }
-
- 
-
-  public static Intake getInstance() {
-    if (_instance == null) {
-            _instance = new Intake();
-    }
-    return _instance;
-  }
+  return _instance;
+}
 /**
- * @return Hinge encoder position. 
- */
-  public double getEncoder() {
-    return hingEncoder.getPosition();
-  }
+* @return Hinge encoder position. 
+*/
+public double getEncoder() {
+  return revboreEncoder.getPosition();
+}
 
-  public void zeroHinge() {
-  }
+public void zeroHinge() {
+}
 /**
- * Sets the hinge power of the left and right motor.
- * @param leftPower Power of the left hinge motor.
- * @param rightPower Power of the right hinge motor.
- */
-  public void setHinge(double leftPower, double rightPower) {
-    leftHinge.set(leftPower);
-  }
+* Sets the hinge power of the left and right motor.
+* @param leftPower Power of the left hinge motor.
+* @param rightPower Power of the right hinge motor.
+*/
+public void setHinge(double leftPower, double rightPower) {
+  intakeInhale.set(leftPower);
+}
 
-  public void runIntake() {
-    intake.set(stateController.getIntakeSpeed());
-  }
-  /** Sets speed of the intake. */
-  public void runIntake(double speed) {
-    intake.set(speed);
-  }
-  /** Vomits the game pieces out. */
-  public void vomit() {
-    intake.set(Constants.IntakeConstants.vomitSpeed);
-  }
-  /** Stops intake. */
-  public void stopIntake() {
-    intake.set(0);
-  }
-  /** Sets hinge to a position with PID looping. */
-  public void hingeTo(double position) {
-    hingePIDController.setReference(position, CANSparkMax.ControlType.kPosition);
-  }
-  /** gets the state for the intake sensor. */
-  public boolean getIntakeSensor() {
-    return !sensor.get();
-  }
+public void runIntake() {
+}
+/** Sets speed of the intake. */
+public void runIntake(double speed) {
+  intake.set(speed);
+}
+/** Vomits the game pieces out. */
+public void vomit() {
+  intake.set(Constants.IntakeConstants.vomitSpeed);
+}
+/** Stops intake. */
+public void stopIntake() {
+  intake.set(0);
+}
+/** Sets hinge to a position with PID looping. */
+public void hingeTo(double position) {
+  hingePIDController.setReference(position, CANSparkMax.ControlType.kPosition);
+}
+/** gets the state for the intake sensor. */
+public boolean getIntakeSensor() {
+  return !sensor.get();
+}
+
+
+
   
+
+   /**
+   * Puts rev bore encoder positions on smartdashboard.
+   * Puts intake sensor state on smartdashboard.
+   */
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    SmartDashboard.putNumber("Encoder Position", getEncoder());
+    SmartDashboard.putBoolean("intake sensor", getIntakeSensor());
+  }
+}
+    // This method will be called once per scheduler run
+
+
+    
